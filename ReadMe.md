a07946e2b1767fb7006a10dd86efeda9e923a72b
Taha Elsayed <tahasakr002004@gmail.com>
2023-01-18 02:15:08 +0100

    Erstellt den Grund-Bausteine für BANKIGO Project

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+#BankiGo-Marketing
---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+/***************************Task_1***************************/
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+console.log(btnsOpenModal);
+const openModal = function (event) {
+  event.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function (event) {
+  event.preventDefault();
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal.forEach(function (currentButton, index) {
+  currentButton.addEventListener('click', openModal);
+});
+// btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
